94893ee2053ff989908854f66f38b4192eb56c25
lautarograciani <lautaro@graciani.ar>
2023-01-25 12:54:20 -0300

    added route for unassigning

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :events
   resources :aliadas
   post '/events/:id/assign_event', to: 'assign_event#assign_event'
+  post '/events/:id/unassign_event', to: 'assign_event#unassign_event'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
